c866e2c2c44e5f6982b8b3f12714e548abc39f70
kauedesousa <desousa.kaue@gmail.com>
2019-04-24 11:23:57 +0200

    environmental_indices

---
+++ R/ETo.R
@@ -0,0 +1,69 @@
+#' Evapotranspiration
+#' 
+#' Compute evapotranspiration using the Blaney-Criddle method
+#' 
+#' @inheritParams temperature
+#' @param lat the latitude (in Decimal degrees)
+#' @param Kc the crop factor for water requirement
+#' @param p optional, a numeric value (from 0 to 1) used if lat is not given,
+#' representing the mean daily percentage of annual daytime hours 
+#' for different latitudes
+#' @return The evapotranspiration in mm/day
+#' @examples
+#'  
+#' # Use temperature data from MODIS MYD11A1
+#' data("breadwheat_modis", package = "gosset")
+#' data("breadwheat", package = "gosset")
+#' 
+#' # the evapotranspiration in the first 100 days after planting
+#' ETo(breadwheat_modis, 
+#'     day.one = breadwheat$planting_date,
+#'     span = 100,
+#'     lat = breadwheat$lat)
+#' 
+#' @export
+ETo <- function(object, day.one = NULL, span = NULL, 
+                lat = NULL, Kc = 1, p = NULL){
+  
+  # get p if lat is provided
+  if (!is.null(lat)) {
+    l <- .round5(lat, 5)
+    m <- as.integer(format(day.one, "%m"))
+    p <- daylight[cbind(match(l , daylight[, 1]), match(m , names(daylight)))]
+  } 
+  
+  if (is.null(p)) {
+    p <- 0.27
+  }
+  
+  # get timespan for the day temperature
+  if (dim(object)[2] == 2) {
+    cat("fetching NASA POWER, this may take a little longer. \n")
+    day <- .get_timespan(object, day.one, span, pars = "T2M_MAX")
+  } else {
+    day <- .get_timespan(object[, , 1], day.one, span)
+  }
+  
+  # get timespan for the night temperature
+  if (dim(object)[2] == 2) {
+    night <- .get_timespan(object, day.one, span, pars = "T2M_MIN")
+  } else {
+    night <- .get_timespan(object[, , 2], day.one, span)
+  }
+  
+  # calculate Tmean
+  Tmean <- (rowMeans(day, na.rm = TRUE) +  rowMeans(night, na.rm = TRUE)) / 2
+  
+  # evapotranspiration
+  eto <- p * (0.46 * Tmean + 8) * Kc
+  
+  return(tibble::tibble(ETo = eto))
+  
+}
+
+# Round to the nearest base value
+.round5 <- function(x, base.value) {
+  
+  base.value * round( x / base.value )
+  
+}
---
+++ R/GDD.R
@@ -0,0 +1,58 @@
+#' Growing Degree Days
+#' 
+#' Compute number of days required to reach growing degree days.
+#' GDD are calculated by taking the integral of warmth above a base temperature.
+#' 
+#' @param degree.days an integer for the degree days required by the crop (look at the physiology of the focal crop)
+#' @param base an integer for the base temperature. Set 10 as default.
+#' @inheritParams temperature
+#' @return The number of days required to reach the growing degree days.
+#' @examples
+#' #' Example 1
+# @export
+GDD <- function(object, day.one = NULL, degree.days = NULL,
+                base = NULL, span = NULL)
+{
+  
+  # validate parameters
+  if (is.null(day.one)) {
+    stop("a Date vector for day.one is required \n")
+  }
+  if (is.null(degree.days)) {
+    stop("an integer for degree.days is required \n")
+  }
+  if (is.null(base)) {
+    base <- 10
+  }
+  if (is.null(span)) {
+    span <- 150
+  }
+  
+  # get timespan for the day temperature
+  if (dim(object)[2] == 2) {
+    day <- get_timespan(object, day.one, span, pars = "T2M_MAX")
+  } else {
+    day <- get_timespan(object[, , 1], day.one, span)
+  }
+  
+  # get timespan for the night temperature
+  if (dim(object)[2] == 2) {
+    night <- get_timespan(object, day.one, span, pars = "T2M_MIN")
+  } else {
+    night <- get_timespan(object[,,2], day.one, span)
+  }
+  
+  # get the difference between day and night temperature
+  Y <- (((day + night) / 2) - base)
+  
+  # sum temperature values until reach the defined degree days
+  Y <- apply(Y, 1, function(x){
+    for(d in 1:length(x)){
+      i=d
+      if(sum(x[1:d]) > degree.days) break}
+    return(i)
+  })
+  
+  return(tibble::tibble(GDD=Y))
+}
+
